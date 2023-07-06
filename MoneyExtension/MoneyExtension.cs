922e85b684c00359d266219f4fd624e4f95441de
Valmir de Lima <valmirblima@hotmail.com>
2023-07-06 15:42:34 -0300

    Finalizada a primeira versao

---
+++ MoneyExtension/MoneyExtension.cs
@@ -0,0 +1,15 @@
+﻿namespace MoneyExtension;
+
+public static class Money
+{
+    public static int ToCents(this decimal amount)
+    {
+        if (amount <= 0)
+            return 0;
+
+        var text = amount.ToString("N2").Replace(",", "").Replace(".", "");
+
+        int.TryParse(text, out var result);
+        return result;
+    }
+}
